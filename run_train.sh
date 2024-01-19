7760f42d540a3adb0943ee6efa2b815d5e4a332b
kamalan <chiajuichou@nycu.edu.tw>
2024-01-19 12:49:47 +0800

    Try to run train in projects/train not using luigi

--- run_train.sh
+++ run_train.sh
@@ -5,8 +5,8 @@
 export DEEPCLEAN_IFO=K1
 #export DEEPCLEAN_STRAIN_CHANNEL=CAL-CS_PROC_DARM_STRAIN_DBL_DQ
 export GPU_INDEX=0
-poetry run law run deepclean.tasks.Train \
+nohup poetry run law run deepclean.tasks.Train \
 	--image train.sif \
 	--gpus $GPU_INDEX \
 	--data-fname $DATA_DIR/K-K1_lldata-1369291863-16384.hdf5 \
-	--output-dir $RESULTS_DIR/my-first-run_K1
+	--output-dir $RESULTS_DIR/test_train > test_train.out &

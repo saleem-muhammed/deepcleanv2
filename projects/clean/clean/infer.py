efcba71098cac82326a38b9d3e0479e9e1a398f6
Muhammed Saleem <muhammed.saleem@ligo.org>
2024-07-12 02:33:43 -0500

    overall reorganizing under projects/clean

--- projects/clean/clean/infer.py
+++ projects/clean/clean/infer.py
@@ -23,30 +23,28 @@ class OnlineInference:
         - writes the cleaned frames   
     """
 
-    def __init__(self, dataset, model, y_scaler, bandpass, outdir, device):
+    def __init__(self, dataset, model, outdir, device):
         self.__logger = logging.getLogger("DeepClean Online Inference")
         self.model = model
         self.device = device
-        self.y_scaler = y_scaler
-        self.bandpass = bandpass
         self.dataset = dataset
         self.outdir = outdir
-        self.edge2crop = int(1.0* self.dataset.hparams.sample_rate)
+        self.edge2crop = int(1.0* self.model.sample_rate)
 
         if not os.path.exists(outdir):
             os.makedirs(self.outdir)
         
     def predict(self):
         witness = list(self.dataset.X_inference)[0]
-        self.pred = self.model(witness.to(self.device))
+        self.pred = self.model.model(witness.to(self.device))
 
     def postprocess(self):
-        self.noise = self.y_scaler(self.pred.cpu(), reverse=True)
-        self.noise = self.bandpass(self.noise.cpu().detach().numpy())
+        self.noise = self.model.y_scaler(self.pred.cpu(), reverse=True)
+        self.noise = self.model.bandpass(self.noise.cpu().detach().numpy())
         self.noise = torch.tensor(self.noise, device=self.device).flatten()
         self.noise = self.noise[self.edge2crop:-self.edge2crop]
         self.raw   = list(self.dataset.y_inference)[0].to(self.device).flatten()
-        self.raw   = self.y_scaler(self.raw.cpu(), reverse=True)
+        self.raw   = self.model.y_scaler(self.raw.cpu(), reverse=True)
         self.raw   = self.raw[self.edge2crop:-self.edge2crop]
         self.raw   = self.raw.to(self.device)  
 
@@ -54,7 +52,7 @@ class OnlineInference:
         cleaned = self.raw - self.noise
         cleaned = cleaned.cpu().numpy()
         gpstime = self.dataset.hoft_crawler.t0 - 1
-        ts = TimeSeries(data=cleaned, t0=gpstime, sample_rate=self.dataset.hparams.sample_rate,
+        ts = TimeSeries(data=cleaned, t0=gpstime, sample_rate=self.model.sample_rate,
                 channel=self.dataset.strain_channel+"_DC", unit='seconds')
         fname = self.dataset.hoft_crawler.file_format.get_name(timestamp=gpstime, length=1)
         filepath = os.path.join(self.outdir, fname)
---
+++ projects/clean/run-clean.sh
@@ -0,0 +1,2 @@
+poetry run python clean/cli.py --config ../../myprojects/60Hz-O3-MDC/config_clean.yaml 
+

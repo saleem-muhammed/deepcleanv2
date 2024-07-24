493cc6c93f2ef3f6ef1f9be105366b7c84de29de
Chia-Jui Chou <agoodmanjerry@gmail.com>
2024-07-23 19:59:03 -0700

    Modify /train.sh for trianing test.

--- train.sh
+++ train.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0
-export DEEPCLEAN_IFO=H1
-export GPU_INDEX=0
+# Directories
+export DEEPCLEAN_CONTAINER_ROOT=~/images/deepclean
+export DATA_DIR=~/dc-demo/data
+export RESULTS_DIR=~/dc-demo/results
+# GPU Settings
+export CUDA_VISIBLE_DEVICES=2
+export GPU_INDEX=2
 export CUDA_LAUNCH_BLOCKING=1
+# Run Training Task
+export DEEPCLEAN_IFO=H1
 poetry run law run deepclean.tasks.Train \
     --image train.sif \
     --gpus $GPU_INDEX \
-    --data-fname $DATA_DIR/O3_AC_clean_H1-1242962000-4096.hdf5 \
-    --train-config ${HOME}/deepcleanv2/projects/train/config-test.yaml \
-    --output-dir ${RESULTS_DIR}/test-O3-H1 \
+    --data-fname $DATA_DIR/O3_AC_train_H1-1250916844-12288.hdf5 \
+    --train-config ${HOME}/deepcleanv2/projects/train/config.yaml \
+    --output-dir ${RESULTS_DIR}/train-O3-H1 \

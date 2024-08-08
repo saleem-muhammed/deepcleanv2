89539876aca725e7852c5564136ad39490d4f1d2
Muhammed Saleem <muhammed.saleem@ligo.org>
2024-08-07 23:46:19 -0500

    asdr logs added

--- projects/clean/clean/infer.py
+++ projects/clean/clean/infer.py
@@ -3,6 +3,7 @@ import torch # type: ignore
 from gwpy.timeseries import TimeSeries, TimeSeriesDict # type: ignore
 import logging
 import numpy as np # type: ignore
+from datetime import datetime
 
 #from train.model import DeepClean
 #from train.architectures import Autoencoder
@@ -30,10 +31,15 @@ class OnlineInference:
         self.device = device
         self.dataset = dataset
         self.outdir = outdir
+        self.logdir = os.path.join(self.outdir, "logs")
         self.edge2crop = int(1.0* self.model.sample_rate)
+        self.current_log_file = None
+        self.current_date = None
 
         if not os.path.exists(outdir):
             os.makedirs(self.outdir)
+        if not os.path.exists(self.logdir):
+            os.makedirs(self.logdir)
         
     def predict(self):
         witness = list(self.dataset.X_inference)[0]
@@ -52,6 +58,49 @@ class OnlineInference:
         self.raw   = self.raw[self.edge2crop:-self.edge2crop]
         self.raw   = self.raw.to(self.device) 
 
+        self.cleaned = self.raw - self.noise
+
+        self.set_fname()
+
+        self.ts = self.make_gwpy_TimeSeries(
+                data = self.cleaned, 
+                channel = self.dataset.strain_channel+"_DC")
+        
+        self.ts_raw   = self.make_gwpy_TimeSeries(
+                 data = self.raw, 
+                 channel = self.dataset.strain_channel)
+        
+        # ts_noise   = self.make_gwpy_TimeSeries(
+        #         data = self.noise, 
+        #         channel = self.dataset.strain_channel+"_pred")
+
+    def compute_asdr(self):
+        asd_cleaned  = self.ts.asd(fftlength=1,overlap=0.0,method='median')
+        asd_raw      = self.ts_raw.asd(fftlength=1,overlap=0.0,method='median')
+        asd_cleaned_inband = asd_cleaned.crop(self.model.freq_low, self.model.freq_high)
+        asd_raw_inband = asd_raw.crop(self.model.freq_low, self.model.freq_high)
+        asd_ratio = (asd_cleaned_inband/asd_raw_inband).value
+        self.mean_asd_ratio = np.mean(asd_ratio)
+        self.max_asd_ratio = max(asd_ratio)
+
+        
+    def write_logs(self):
+        self.compute_asdr()
+        
+        current_date = datetime.utcnow().strftime('%Y-%m-%d')
+        
+        if current_date != self.current_date:
+            if self.current_log_file:
+                self.current_log_file.close()
+                
+            log_filename = os.path.join(self.logdir, f"asdr_{current_date}.log")
+            self.current_log_file = open(log_filename, 'a')
+            self.current_date = current_date
+        
+        log_entry = f"{self.output_t0}, {datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')} - Mean ASDR: {self.mean_asd_ratio}, Max ASDR: {self.max_asd_ratio}\n"
+        self.current_log_file.write(log_entry)
+        self.current_log_file.flush()
+
     def set_fname(self):
         self.output_t0 = self.dataset.t0
         self.fname = f"{self.dataset.prefix}-{int(self.output_t0)}-{int(self.dataset.duration)}.gwf"
@@ -61,30 +110,18 @@ class OnlineInference:
         data = np.array(data.cpu().numpy(), dtype=np.float64)
         ts = TimeSeries(data=data, t0=self.output_t0, sample_rate=self.model.sample_rate,
                 channel=channel, unit='seconds') 
-        return ts       
+        return ts 
         
 
     def write(self):
-        self.set_fname()
-        cleaned = self.raw - self.noise
+        
         #cleaned = cleaned.cpu().numpy()
 
-        ts = self.make_gwpy_TimeSeries(
-                data = cleaned, 
-                channel = self.dataset.strain_channel+"_DC")
-        
-        # ts_raw   = self.make_gwpy_TimeSeries(
-        #         data = self.raw, 
-        #         channel = self.dataset.strain_channel)
-        
-        # ts_noise   = self.make_gwpy_TimeSeries(
-        #         data = self.noise, 
-        #         channel = self.dataset.strain_channel+"_pred")
 
         # Create a TimeSeriesDict and add the TimeSeries objects to it
         ts_dict = TimeSeriesDict()
-        ts_dict[self.dataset.strain_channel+"_DC"] = ts
-        # ts_dict[self.dataset.strain_channel] = ts_raw
+        ts_dict[self.dataset.strain_channel+"_DC"] = self.ts
+        # ts_dict[self.dataset.strain_channel] = self.ts_raw
         # ts_dict[self.dataset.strain_channel+"_pred"] = ts_noise
 
         # Write the TimeSeriesDict to a file
@@ -98,3 +135,4 @@ class OnlineInference:
         self.predict()
         self.postprocess()
         self.write()
+        self.write_logs()

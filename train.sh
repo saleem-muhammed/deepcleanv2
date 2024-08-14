4a1ccd5087da147248054025bbb8fd556358cd41
Chia-Jui Chou <chiajui.chou@ldas-grid.ligo-la.caltech.edu>
2024-08-14 06:20:48 -0500

    Train on kernel_size=3

--- train.sh
+++ train.sh
@@ -14,4 +14,4 @@ poetry run law run deepclean.tasks.Train \
     --gpus $GPU_INDEX \
     --data-fname $DATA_DIR/O3_AC_train_H1-1250916844-12288.hdf5 \
     --train-config ${HOME}/deepcleanv2/projects/train/config.yaml \
-    --output-dir ${RESULTS_DIR}/train-O3-H1 \
+    --output-dir ${RESULTS_DIR}/train-O3-H1-train_kernel_3 \
